c971f2101a25f922712ca4517fb3848df9123e38
zach brecht <brechtz32@gmail.com>
2023-12-08 22:51:31 -0600

    initial commit variables exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string petName;
+            petName = "Charlie";
+
+            int petAge = 6;
+
+            char middleInitial = 'E';
+
+            bool isBig = true;
+
+            double petWeight = 110.5;
+
+            decimal petHeight = 32.5m;
+
+            Console.WriteLine($"Hello! I have a dog and his name is {petName}. {petName} {middleInitial} Porkchop. He is {petAge} years old.");
+            Console.WriteLine($"{petName} weighs {petWeight} lbs. and is {petHeight} inches tall. Is {petName} big? {isBig}!");
+
         }
     }
 }
